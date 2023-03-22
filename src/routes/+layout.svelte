1e1c78011fba530859f31f43d2a5f9235da21c07
David Will <64697174+LtSalt@users.noreply.github.com>
2023-03-22 02:12:04 +0100

    removed layout.svelte

--- src/routes/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<slot></slot>
-
-<style>
-    @import 'leaflet/dist/leaflet.css';
-    @import "@fontsource/montserrat/400.css";
-    @import "@fontsource/montserrat/500.css";
-    @import "@fontsource/montserrat/700.css";
-    @import "../styles.css";
-</style>
